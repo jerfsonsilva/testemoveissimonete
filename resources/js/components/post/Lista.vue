b24dac16f2e122aa61829a79ced69ceb49fee136
jerfsonsilva <jerfsonlink@gmail.com>
2021-03-24 22:37:54 -0300

    Criar tela de posts

--- resources/js/components/post/Lista.vue
+++ resources/js/components/post/Lista.vue
@@ -1,25 +1,43 @@
 <template>
-   
 
+    <div class="row">
+
+        <div class="col-sm-6" v-for="item in lista">
+            <div class="card card-post"  >
+              <div class="card-body row">
+                <img class="col-4" :src="item.foto" width="200" >
+
+                <div class="col-8 text-right">
+                    <h5 class="card-title text-left">{{item.autor}}</h5>
+                    <h2 class="card-title text-left">{{item.titulo}}</h2>
+                    <p class="card-text text-left">{{item.conteudo.substring(0,100)}}</p>
+                    <a :href="'/post/'+item.id" class="btn btn-primary link">Acessar > > </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
     // <ContactsList />
    // import ContactsList from './ContactsList';
    export default {
-    props: ['input_name'],
+    props: [],
     data() {
         return {
-            mostrar:false,
+            lista:[],
         };
     },
     mounted() {
-     axios.get()
-     .then((response) => {
-        
-     });
- },
- methods: {
+       axios.get('/artigos')
+       .then((response) => {
+
+        if(response.data)
+            this.lista = response.data.itens;
+    });
+   },
+   methods: {
  }//,    components: { Cabecalho,ListaContato,Conversa}
 }
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
 			<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="#">Posts</a>
+						<a class="nav-link" href="/">Posts</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Contato</a>
